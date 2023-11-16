1273bb5bdcabcd2347d71a5658b76a3613cdf12a
Kamil Data <kamildata2@gmail.com>
2023-11-16 15:15:25 +0100

    Add homepage

--- AngularApp/src/app/public/public.module.ts
+++ AngularApp/src/app/public/public.module.ts
@@ -7,9 +7,10 @@ import { PublicRoutingModule } from './public-routing.module';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { BrowseTalentsComponent } from './components/browse-talents/browse-talents.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [HomepageComponent, BrowseTalentsComponent, ContactComponent],
-  imports: [CommonModule, PublicRoutingModule, RouterModule],
+  imports: [CommonModule, PublicRoutingModule, RouterModule, ButtonModule],
 })
 export class PublicModule {}
